5c5836d9e0661eb162172e3c118a1c549a9be5a4
Simon Lam <thesimonlam@gmail.com>
2025-02-26 20:17:32 +0000

    fix menu

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -4,21 +4,9 @@ import "./Menu.css";
 class Menu extends Component {
   render() {
     return (
-      <div style={{ height: "100vh" }}>
-        <object
-          data="https://sunshine-takeaway-menu-pdf.s3.eu-west-1.amazonaws.com/sunshine-takeaway-menu.pdf"
-          type="application/pdf"
-          width="100%"
-          height="100%"
-        >
-          <p>
-            It appears you don't have a PDF plugin for this browser. No
-            biggie... you can{" "}
-            <a href="https://sunshine-takeaway-menu-pdf.s3.eu-west-1.amazonaws.com/sunshine-takeaway-menu.pdf">
-              click here to download the PDF file.
-            </a>
-          </p>
-        </object>
+      <div style={{ width: "100%" }}>
+        <img style={{ width: "100%" }} src="https://sunshine-takeaway-menu-pdf.s3.eu-west-1.amazonaws.com/menu-1.png"></img>
+        <img style={{ width: "100%" }} src="https://sunshine-takeaway-menu-pdf.s3.eu-west-1.amazonaws.com/menu-2.png"></img>
       </div>
     );
   }
